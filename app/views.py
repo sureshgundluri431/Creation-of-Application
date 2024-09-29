bc9dfe62f5ca28d5689eb1b2157068eeea6a6c27
sureshgundluri431 <iloveusuresh431@gmail.com>
2024-09-29 16:38:31 +0530

    Application is Created

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def App_Creation(request):
+    return HttpResponse('App Creation in the Django-Project')
